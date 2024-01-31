5b253870504fb4fcda0f5b6c1555a77c499ae8b5
PriyathamVarma <priyatham002@gmail.com>
2024-01-31 17:04:05 +0000

    🟢 ADD : favcon icon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import AppIconImg from "../../public/images/app_icon.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Back3nd",
-  description: "Main UI for back3nd",
+  description: "AWS for FinTech using blockchain",
+  icons: [{ rel: "icon", url: AppIconImg.src }],
 };
 
 export default function RootLayout({
